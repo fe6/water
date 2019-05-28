f729c9f80fbfd7ff0161783c3751b79b5cea3996
李梦龙 <649076408@qq.com>
2019-05-28 17:22:15 +0800

    revert($affix): modify test

--- src/components/affix/affix.test.ts
+++ src/components/affix/affix.test.ts
@@ -3,12 +3,20 @@ import Affix from './Affix.vue';
 
 describe('Affix.vue', () => {
   let wrapperBottom:any = null;
+  let wrapperOffsetValue:any = null;
   let wrapperbeforeDestroy:any = null;
 
   beforeEach(() => {
+    wrapperOffsetValue = mount(Affix, {
+      propsData: {
+        offsetTop: 20,
+        destroy: false,
+      },
+      attachToDocument: true,
+    });
     wrapperBottom = mount(Affix, {
       propsData: {
-        offsetBottom: 10,
+        offsetBottom: 110,
       },
       attachToDocument: true,
     });
@@ -24,6 +32,21 @@ describe('Affix.vue', () => {
           wrapperBottom.vm.updatePostion();
           expect(wrapperBottom.vm.affixStyle).toBe('');
           expect(wrapperBottom.vm.offsetType).toBe('bottom');
+          expect(wrapperBottom.vm.offsetValue).toBe(110);
+          done();
+        }, 0);
+      } catch (err) {
+        done.fail(err);
+      }
+    });
+  });
+
+  it('test offsetValue', (done) => {
+    wrapperOffsetValue.vm.$nextTick(() => {
+      try {
+        setTimeout(() => {
+          expect(wrapperOffsetValue.vm.offsetValue).toBe(20);
+          wrapperOffsetValue.destroy();
           done();
         }, 0);
       } catch (err) {
@@ -35,6 +58,7 @@ describe('Affix.vue', () => {
   it('test destroy', (done) => {
     wrapperbeforeDestroy.vm.$nextTick(() => {
       try {
+        expect(wrapperbeforeDestroy.vm.offsetValue).toBe(10);
         const debounceFn = jest.fn();
         wrapperbeforeDestroy.vm.target.removeEventListener = debounceFn;
         wrapperbeforeDestroy.destroy();
