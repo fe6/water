05462b5c11a350274cc7a04a35bfc1af232d4e0d
李梦龙 <649076408@qq.com>
2019-06-17 10:28:44 +0800

    style($ui): modify some style

--- src/views/code/link/size.ts
+++ src/views/code/link/size.ts
@@ -3,11 +3,11 @@ import Icon from '@/components/icon/Icon.vue';
 import { ApiEntity } from '@/views/entity/demoentity';
 import { upperFirst } from 'lodash/string';
 
-const sizes = ['small', '', 'large'];
+const sizes = ['', 'large'];
 
 const base: ApiEntity = {
   title: '超链尺寸',
-  desc: '超链分大中小三种尺寸。通过属性 <code>size</code> 进行设置，可选值有 <code>large</code> 或 <code>small</code> 。',
+  desc: '超链分两种尺寸。通过属性 <code>size</code> 进行设置，可选值有 <code>large</code> 。',
   code: sizes.reduce((acc: string, size: string, index: number) => `${acc}<${Link.name} class="button" size="${size}">这是个超链 ${upperFirst(size) || 'Default'}</${Link.name}>${index < sizes.length - 1 ? '\n' : ''}`, ''),
   render: (h: Function) => h('section', {
     class: 'demo-demo',
