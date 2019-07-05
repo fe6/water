254868ea90a80725f5b32b7b69dfe341df5ae08e
李梦龙 <649076408@qq.com>
2019-07-05 11:21:23 +0800

    chore($enter): config enter file

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,15 +5,23 @@ import Breadcrumb from './breadcrumb/Breadcrumb.vue';
 import BreadcrumbItem from './breadcrumb/BreadcrumbItem.vue';
 import Button from './button/Button.vue';
 import ButtonGroup from './button/ButtonGroup.vue';
+import Cascader from './cascader/Cascader.vue';
 import Checkbox from './checkbox/Checkbox.vue';
 import CheckboxGroup from './checkbox/CheckboxGroup.vue';
+import Empty from './empty/Empty.vue';
 import Row from './grid/Row.vue';
 import Col from './grid/Col.vue';
 import Icon from './icon/Icon.vue';
 import Input from './input/Input.vue';
+import InputNumber from './inputnumber/InputNumber.vue';
 import Link from './link/Link.vue';
+import Modal from './modal/Modal.vue';
 import Radio from './radio/Radio.vue';
 import RadioGroup from './radio/RadioGroup.vue';
+import Select from './select/Select.vue';
+import Spin from './spin/Spin.vue';
+import Switch from './switch/Switch.vue';
+import Tooltip from './tooltip/Tooltip.vue';
 
 const waterCpt = {
   Affix,
@@ -22,15 +30,23 @@ const waterCpt = {
   BreadcrumbItem,
   Button,
   ButtonGroup,
+  Cascader,
   Checkbox,
   CheckboxGroup,
+  Empty,
   Row,
   Col,
   Icon,
   Input,
+  InputNumber,
   Link,
+  Modal,
   Radio,
   RadioGroup,
+  Select,
+  Spin,
+  Switch,
+  Tooltip,
 };
 
 const install = (Vue: any) => {
