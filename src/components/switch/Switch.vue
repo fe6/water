0a71a6973a450221551c4bfae8430d20c28dd4f1
李梦龙 <649076408@qq.com>
2019-07-13 00:49:31 +0800

    fix($switch): modify change function type

--- src/components/switch/Switch.vue
+++ src/components/switch/Switch.vue
@@ -33,40 +33,6 @@ export interface ReturnParamsEntity {
   [propName: string]: any;
 }
 
-interface Function1 {
-    /**
-      * Calls the function, substituting the specified object for the this value of the function, and the specified array for the arguments of the function.
-      * @param thisArg The object to be used as the this object.
-      * @param argArray A set of arguments to be passed to the function.
-      */
-    apply(this: Function, thisArg: any, argArray?: any): any;
-
-    /**
-      * Calls a method of an object, substituting another object for the current object.
-      * @param thisArg The object to be used as the current object.
-      * @param argArray A list of arguments to be passed to the method.
-      */
-    call(this: Function, thisArg: any, ...argArray: any[]): any;
-
-    /**
-      * For a given function, creates a bound function that has the same body as the original function.
-      * The this object of the bound function is associated with the specified object, and has the specified initial parameters.
-      * @param thisArg An object to which the this keyword can refer inside the new function.
-      * @param argArray A list of arguments to be passed to the new function.
-      */
-    bind(this: Function, thisArg: any, ...argArray: any[]): any;
-
-    /** Returns a string representation of a function. */
-    toString(): string;
-
-    prototype: any;
-    readonly length: number;
-
-    // Non-standard extensions
-    arguments: any;
-    caller: Function;
-}
-
 @Component
 export default class WSwitch extends Vue {
   name: string = 'Switch';
@@ -97,7 +63,7 @@ export default class WSwitch extends Vue {
   @Prop({
     type: Function,
     default: () => {},
-  }) private change!: Function1;
+  }) private change!: <T>(arg: T) => T;
 
   mounted() {
     this.setStatus(this.value);
