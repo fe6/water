a743ae1a54bdd4e6b630c69f1953034f09357177
李梦龙 <649076408@qq.com>
2019-11-12 20:46:46 +0800

    style($site): mdoify layout

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="layout">
+    <WAffix
+      :offsetTop="0"
+    >
     <header class="layout-header">
       <WRow>
         <WCol :span="24" class="layout-header-col">
@@ -20,62 +23,58 @@
         </WCol>
       </WRow>
     </header>
+    </WAffix>
     <div class="layout-main">
-      <WRow>
-        <WCol :xs="12" :sm="10" :md="8" :lg="6" :xl="5" :xxl="4">
-          <section
-            class="layout-menu"
-            :style="menuStyle"
-          >
-            <template v-for="(navItem, navIndex) in nav">
-              <div
-                class="layout-menu-sub"
-                :title="navItem.title"
-                :key="`navItem_${navIndex}`"
-                v-if="navItem.children"
-              >{{navItem.title}}</div>
+      <section
+        class="layout-menu"
+        :style="menuStyle"
+      >
+        <template v-for="(navItem, navIndex) in nav">
+          <div
+            class="layout-menu-sub"
+            :title="navItem.title"
+            :key="`navItem_${navIndex}`"
+            v-if="navItem.children"
+          >{{navItem.title}}</div>
+          <router-link
+            exact-active-class="layout-menu-on"
+            :to="navItem.to"
+            class="layout-menu-link"
+            :key="`navItem_${navIndex}`"
+            v-else
+          >{{navItem.title}}</router-link>
+          <template v-for="(navChildren, navChildrenIndex) in navItem.children">
+            <div
+              class="layout-menu-h3"
+              :title="navChildren.title"
+              :key="`navChildren_${navChildrenIndex}`"
+            >{{navChildren.title}}</div>
+            <ul
+              class="layout-menu-list"
+              :key="navChildrenIndex"
+            >
               <router-link
                 exact-active-class="layout-menu-on"
-                :to="navItem.to"
-                class="layout-menu-link"
-                :key="`navItem_${navIndex}`"
-                v-else
-              >{{navItem.title}}</router-link>
-              <template v-for="(navChildren, navChildrenIndex) in navItem.children">
-                <div
-                  class="layout-menu-h3"
-                  :title="navChildren.title"
-                  :key="`navChildren_${navChildrenIndex}`"
-                >{{navChildren.title}}</div>
-                <ul
-                  class="layout-menu-list"
-                  :key="navChildrenIndex"
-                >
-                  <router-link
-                    exact-active-class="layout-menu-on"
-                    tag="li"
-                    :to="linkItem.to"
-                    class="layout-menu-item"
-                    v-for="(linkItem, linkIndex) in navChildren.nav"
-                    :key="`linkItem_${linkIndex}`"
-                  >{{linkItem.title}}</router-link>
-                </ul>
-              </template>
-            </template>
-          </section>
-        </WCol>
-        <WCol :xs="12" :sm="16" :md="17" :lg="18" :xl="20">
-          <article class="layout-cantainer">
-            <router-view/>
-          </article>
-        </WCol>
-      </WRow>
+                tag="li"
+                :to="linkItem.to"
+                class="layout-menu-item"
+                v-for="(linkItem, linkIndex) in navChildren.nav"
+                :key="`linkItem_${linkIndex}`"
+              >{{linkItem.title}}</router-link>
+            </ul>
+          </template>
+        </template>
+      </section>
+      <article class="layout-cantainer">
+        <router-view/>
+      </article>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Watch, Vue } from 'vue-property-decorator';
+import WAffix from '@/components/affix/Affix.vue';
 import WRow from '@/components/grid/Row.vue';
 import WCol from '@/components/grid/Col.vue';
 import { nav, NavEntity } from '@/nav';
@@ -86,6 +85,7 @@ interface MenuStyleEntity {
 
 @Component({
   components: {
+    WAffix,
     WRow,
     WCol,
   },
