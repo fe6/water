711619aa9bea422f98a7bd6026e534399b646c0e
李梦龙 <649076408@qq.com>
2020-01-20 10:47:38 +0800

    build($theme): add eslint config

--- packages/theme/gulpfile.js
+++ packages/theme/gulpfile.js
@@ -1,3 +1,5 @@
+/** @format */
+
 const { src, dest, series } = require('gulp');
 const sass = require('gulp-sass');
 const rename = require('gulp-rename');
@@ -7,40 +9,42 @@ const gulpStylelint = require('gulp-stylelint');
 const del = require('del');
 
 const copy = (cb) => {
-  src('./src/**/*.scss')
-    .pipe(dest('./dist/scss'));
+  src('./src/**/*.scss').pipe(dest('./dist/scss'));
   cb();
 };
 
-const clean = () => del(['./dist'])
+const clean = () => del(['./dist']);
 
 const build = (cb) => {
   src(['./src/cpt/*.scss', './src/index.scss'])
-  .pipe(sass())
-  .pipe(autoprefixer({
-    cascade: false
-  }))
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      }),
+    )
     .pipe(dest('./dist/css'))
     .pipe(cssmin())
-    .pipe(rename(function (path) {
-      return {
-        dirname: path.dirname + "/css",
-        basename: path.basename + ".min",
-        extname: ".css"
-      };
-    }))
+    .pipe(
+      rename((path) => {
+        return {
+          dirname: `${path.dirname}/css`,
+          basename: `${path.basename}.min`,
+          extname: '.css',
+        };
+      }),
+    )
     .pipe(dest('./dist/'));
   cb();
-}
+};
 
 exports.styleLint = (cb) => {
-  src('src/**/*.scss')
-    .pipe(gulpStylelint({
-      reporters: [
-        {formatter: 'string', console: true}
-      ],
-    }));
-    cb();
-  }
+  src('src/**/*.scss').pipe(
+    gulpStylelint({
+      reporters: [{ formatter: 'string', console: true }],
+    }),
+  );
+  cb();
+};
 
 exports.default = series(clean, copy, build);
